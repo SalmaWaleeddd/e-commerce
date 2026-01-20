974d467c5218c37d764cd652db6b2bc25f38ff90
Salma Waleed <salmawaleed2030@outlook.com>
2026-01-20 20:46:04 +0200

    feat(routes): implement main route configuration for app navigation

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+// routes/index.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '@/pages/HomePage';
+import ProductDetailPage from '@/pages/ProductDetailsPage';
+import { NotFound } from '@/pages/NotFoundPage';
+
+
+export const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<HomePage />} />
+      <Route path="/products/:id" element={<ProductDetailPage />} />
+      
+
+      
+      {/* 404 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
