3b3376b1ec500795696125b22293b54e353f535b
Salma Waleed <salmawaleed2030@outlook.com>
2026-01-22 14:45:41 +0200

    feat: added coming soon page and update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 // routes/index.tsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '@/pages/HomePage';
-import ProductDetailPage from '@/pages/ProductDetailsPage';
-import { NotFound } from '@/pages/NotFoundPage';
-import CartPage from '@/pages/CartPage';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import ProductDetailPage from "@/pages/ProductDetailsPage";
+import { NotFound } from "@/pages/NotFoundPage";
+import CartPage from "@/pages/CartPage";
+import CreateProductPage from "@/pages/CreateProductPage";
+import ComingSoonPage from "@/pages/ComingSoon";
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -15,9 +16,11 @@ export const AppRoutes: React.FC = () => {
       <Route path="/products" element={<HomePage />} />
       <Route path="/products/:id" element={<ProductDetailPage />} />
       <Route path="/cart" element={<CartPage />} />
-      
+      <Route path="/create-product" element={<CreateProductPage />} />
+      <Route path="/about" element={<ComingSoonPage />} />
+      <Route path="/contact" element={<ComingSoonPage />} />
+      <Route path="/privacy" element={<ComingSoonPage />} />
 
-      
       {/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
